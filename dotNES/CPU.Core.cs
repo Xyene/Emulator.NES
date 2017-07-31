5744a5b03ae3e814c676adfa8f40d42cc6924dc9
Tudor <xyene@dmoj.ca>
2017-07-30 23:48:42 -0400

    Reset sets I bit, not B

--- dotNES/CPU.Core.cs
+++ dotNES/CPU.Core.cs
@@ -18,7 +18,7 @@ namespace dotNES
         public void Reset()
         {
             SP -= 3;
-            F.BreakSource = true;
+            F.InterruptsDisabled = true;
         }
 
         public void ExecuteSingleInstruction()
@@ -30,7 +30,7 @@ namespace dotNES
             ResetInstructionAddressingMode();
 
             // if (cycle >= 4900)
-            Console.WriteLine($"{(PC - 1).ToString("X4")}  {currentInstruction.ToString("X2")}	\t\t\tA:{A.ToString("X2")} X:{X.ToString("X2")} Y:{Y.ToString("X2")} P:{P.ToString("X2")} SP:{SP.ToString("X2")} CYC:{_cycle}");
+            Console.WriteLine($"{(PC - 1).ToString("X4")}  {currentInstruction.ToString("X2")}	{opcodeNames[currentInstruction]}\t\t\tA:{A.ToString("X2")} X:{X.ToString("X2")} Y:{Y.ToString("X2")} P:{P.ToString("X2")} SP:{SP.ToString("X2")} CYC:{_cycle}");
 
             Opcode op = opcodes[currentInstruction];
             if (op == null)
