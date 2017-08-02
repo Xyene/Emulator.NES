fdf10fd274b501775b9c3ec06246024941562f97
lilyxy <lilyw357@gmail.com>
2017-08-02 10:19:19 -0400

    Implement PPU memory, possibly

--- dotNES/PPU.Memory.cs
+++ dotNES/PPU.Memory.cs
@@ -8,6 +8,10 @@ namespace dotNES
 {
     partial class PPU
     {
+        private byte[] OAM = new byte[256];
+        private byte[] VRAM = new byte[0x800];
+        private byte[] PaletteRAM = new byte[0x20];
+
         private int _lastWrittenRegister;
 
         public void WriteRegister(int reg, byte val)
@@ -42,15 +46,15 @@ namespace dotNES
             switch (reg)
             {
                 case 0x0000:
-                    return (byte) PPUCTRL;
+                    return (byte)PPUCTRL;
                 case 0x0001:
-                    return (byte) PPUMASK;
+                    return (byte)PPUMASK;
                 case 0x0002:
-                    return (byte) PPUSTATUS;
+                    return (byte)PPUSTATUS;
                 case 0x0006:
-                    return (byte) PPUADDR;
+                    return (byte)PPUADDR;
                 case 0x0007:
-                    return (byte) PPUDATA;
+                    return (byte)PPUDATA;
             }
             throw new NotImplementedException(reg.ToString("X2"));
         }
@@ -61,10 +65,33 @@ namespace dotNES
             throw new NotImplementedException();
         }
 
-        public void WriteByte(int addr, int val)
+        public void WriteByte(int addr, int _val)
         {
+            byte val = (byte)_val;
+
             addr &= 0xFFFF;
-            throw new NotImplementedException($"{addr.ToString("X4")} = {val.ToString("X2")}");
+            switch (addr & 0xF000)
+            {
+                case 0x0000:
+                case 0x1000:
+                    emulator.Cartridge.CHRROM[addr] = val;
+                    break;
+                case 0x2000:
+                    VRAM[(addr - 0x2000) & 0x7FF] = val;
+                    break;
+                case 0x3000:
+                    if (addr <= 0x3EFF)
+                    {
+                        VRAM[addr - 0x300] = val;
+                    }
+                    else
+                    {
+                        PaletteRAM[(addr - 0x3000) & 0x19] = val;
+                    }
+                    break;
+                default:
+                    throw new NotImplementedException($"{addr.ToString("X4")} = {val.ToString("X2")}");
+            }
         }
     }
 }
