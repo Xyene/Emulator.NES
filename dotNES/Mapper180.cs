122c9f2b542c0a8c02d217518db2ccbaf9d4ef53
Tudor Brindus <xyene@dmoj.ca>
2017-08-27 08:39:53 +0200

    Document Mapper180

--- dotNES/Mapper180.cs
+++ dotNES/Mapper180.cs
@@ -3,6 +3,9 @@ using System.Runtime.CompilerServices;
 
 namespace dotNES
 {
+    // Mapper used strictly for Crazy Climber; logic is slighly different:
+    // $8000-$C000 is fixed to *first* bank
+    // $C000-$FFFF is switchable, controlled the same as UxROM
     class Mapper180 : UxROM
     {
         public Mapper180(Emulator emulator) : base(emulator)
