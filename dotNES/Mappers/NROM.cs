cb2eedcf459260a06721163c834b158abdad003e
Tudor Brindus <xyene@dmoj.ca>
2017-08-30 23:24:54 +0200

    Refactor NROM too

--- dotNES/Mappers/NROM.cs
+++ dotNES/Mappers/NROM.cs
@@ -5,19 +5,19 @@ namespace dotNES.Mappers
 {
     class NROM : AbstractMemory
     {
-        private readonly byte[] AddressSpace = new byte[0x2000 + 0x8000];
+        private readonly byte[] _addressSpace = new byte[0x2000 + 0x8000]; // Space for $2000 VRAM + $8000 PRG
 
         public NROM(Emulator emulator) : base(emulator)
         {
             for (int i = 0; i < 0x8000; i++)
             {
-                int offset = _emulator.Cartridge.PRGROMSize == 16384 ? (i & 0xBFFF) : i;
-                AddressSpace[0x2000 + i] = _emulator.Cartridge.PRGROM[offset];
+                int offset = _emulator.Cartridge.PRGROMSize == 16384 ? i & 0xBFFF : i;
+                _addressSpace[0x2000 + i] = _PRGROM[offset];
             }
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public override uint ReadByte(uint addr) => AddressSpace[(addr & 0xFFFF) - 0x6000];
+        public override uint ReadByte(uint addr) => _addressSpace[(addr & 0xFFFF) - 0x6000];
 
         public override void WriteByte(uint addr, uint _val)
         {
@@ -26,7 +26,7 @@ namespace dotNES.Mappers
             {
                 case 0x6000:
                 case 0x7000:
-                    AddressSpace[addr - 0x6000] = val;
+                    _addressSpace[addr - 0x6000] = val;
                     break;
                 default:
                     throw new NotImplementedException(addr.ToString("X4") + " = " + val.ToString("X2"));
