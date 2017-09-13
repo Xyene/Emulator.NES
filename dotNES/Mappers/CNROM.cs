f923aac82282d0b821f0e0fdb25ea9feb06ee0c7
Tudor Brindus <xyene@dmoj.ca>
2017-09-13 19:08:12 +0200

    Implement CNROM support

---
+++ dotNES/Mappers/CNROM.cs
@@ -0,0 +1,23 @@
+﻿namespace dotNES.Mappers
+{
+    [MapperDef(3)]
+    class CNROM : BaseMapper
+    {
+        protected int _bankOffset;
+
+        public CNROM(Emulator emulator) : base(emulator)
+        {
+        }
+
+        public override void InitializeMemoryMap(PPU ppu)
+        {
+           ppu.MapReadHandler(0x0000, 0x1FFF, addr => _chrROM[_bankOffset + addr]);
+        }
+
+        public override void InitializeMemoryMap(CPU cpu)
+        {
+            cpu.MapReadHandler(0x8000, 0xFFFF, addr => _prgROM[addr - 0x8000]);
+            cpu.MapWriteHandler(0x8000, 0xFFFF, (addr, val) => _bankOffset = (val & 0x3) * 0x2000);
+        }
+    }
+}
