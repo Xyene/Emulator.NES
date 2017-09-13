f7b1e7a8d116e0e5e9d17f0f106af3c1fbdfa205
Tudor Brindus <xyene@dmoj.ca>
2017-09-13 20:29:34 +0200

    Fix CNROM in 16kb mode

--- dotNES/Mappers/CNROM.cs
+++ dotNES/Mappers/CNROM.cs
@@ -11,12 +11,20 @@
 
         public override void InitializeMemoryMap(PPU ppu)
         {
-           ppu.MapReadHandler(0x0000, 0x1FFF, addr => _chrROM[_bankOffset + addr]);
+            ppu.MapReadHandler(0x0000, 0x1FFF, addr => _chrROM[_bankOffset + addr]);
         }
 
         public override void InitializeMemoryMap(CPU cpu)
         {
-            cpu.MapReadHandler(0x8000, 0xFFFF, addr => _prgROM[addr - 0x8000]);
+            if (_prgROM.Length == 0x8000)
+            {
+                cpu.MapReadHandler(0x8000, 0xFFFF, addr => _prgROM[addr - 0x8000]);
+            }
+            else
+            {
+                cpu.MapReadHandler(0x8000, 0xBFFF, addr => _prgROM[addr - 0x8000]);
+                cpu.MapReadHandler(0xC000, 0xFFFF, addr => _prgROM[addr - 0xC000]);
+            }
             cpu.MapWriteHandler(0x8000, 0xFFFF, (addr, val) => _bankOffset = (val & 0x3) * 0x2000);
         }
     }
