68eb9e342ae5fa4301cc2419190682ec51d7a507
Tudor Brindus <xyene@dmoj.ca>
2017-09-14 13:03:44 +0200

    Clean up OpenGL renderer

--- dotNES/Renderers/OpenGLRenderer.cs
+++ dotNES/Renderers/OpenGLRenderer.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Drawing;
-using System.Runtime.InteropServices;
-using System.Windows.Forms;
 using OpenGL;
 
 
@@ -11,6 +9,7 @@ namespace dotNES.Renderers
     {
         private UI _ui;
         private uint _textureId;
+        private readonly Object _drawLock = new Object();
 
         public string RendererName => "OpenGL";
 
@@ -22,48 +21,54 @@ namespace dotNES.Renderers
 
         private void RenderControl_ContextCreated(object sender, GlControlEventArgs e)
         {
-            _textureId = Gl.GenTexture();
-            Gl.BindTexture(TextureTarget.Texture2d, _textureId);
-            Gl.TextureParameterEXT(_textureId, TextureTarget.Texture2d, TextureParameterName.TextureMagFilter, Gl.NEAREST);
-            Gl.TextureParameterEXT(_textureId, TextureTarget.Texture2d, TextureParameterName.TextureMinFilter, Gl.NEAREST);
+            lock (_drawLock)
+            {
+                _textureId = Gl.GenTexture();
+                Gl.BindTexture(TextureTarget.Texture2d, _textureId);
+                Gl.TextureParameterEXT(_textureId, TextureTarget.Texture2d, TextureParameterName.TextureMagFilter, Gl.NEAREST);
+                Gl.TextureParameterEXT(_textureId, TextureTarget.Texture2d, TextureParameterName.TextureMinFilter, Gl.NEAREST);
+            }
         }
 
         private void RenderControl_Render(object sender, GlControlEventArgs e)
         {
-            Control senderControl = (Control)sender;
-
-            Gl.Enable(EnableCap.Texture2d);
-            Gl.ClearColor(Color.Gray.R / 255.0f, Color.Gray.G / 255.0f, Color.Gray.B / 255.0f, 0);
-            Gl.Clear(ClearBufferMask.ColorBufferBit);
-
-            if (_ui.gameStarted)
+            lock (_drawLock)
             {
-                Gl.MatrixMode(MatrixMode.Projection);
-                Gl.LoadIdentity();
+                Gl.Enable(EnableCap.Texture2d);
+                Gl.ClearColor(Color.Gray.R / 255.0f, Color.Gray.G / 255.0f, Color.Gray.B / 255.0f, 0);
+                Gl.Clear(ClearBufferMask.ColorBufferBit);
 
-                Gl.Viewport(0, 0, senderControl.ClientSize.Width, senderControl.ClientSize.Height);
-                Gl.Ortho(0d, 1d, 0d, 1d, -1d, 1d);
-
-                Gl.MatrixMode(MatrixMode.Modelview);
-                Gl.LoadIdentity();
-
-                Gl.BindTexture(TextureTarget.Texture2d, _textureId);
-
-                using (MemoryLock locked = new MemoryLock(_ui.rawBitmap))
+                if (_ui.gameStarted)
                 {
-                    Gl.TexImage2D(TextureTarget.Texture2d, 0, InternalFormat.Rgba, UI.GameWidth, UI.GameHeight, 0, PixelFormat.Bgra, PixelType.UnsignedByte, locked.Address);
+                    Gl.MatrixMode(MatrixMode.Projection);
+                    Gl.LoadIdentity();
+                    Gl.Viewport(0, 0, ClientSize.Width, ClientSize.Height);
+                    Gl.Ortho(0d, 1d, 0d, 1d, -1d, 1d);
+                    Gl.MatrixMode(MatrixMode.Modelview);
+                    Gl.LoadIdentity();
 
+                    Gl.BindTexture(TextureTarget.Texture2d, _textureId);
+
+                    using (MemoryLock locked = new MemoryLock(_ui.rawBitmap))
+                    {
+                        Gl.TexImage2D(TextureTarget.Texture2d, 0, InternalFormat.Rgba, UI.GameWidth, UI.GameHeight, 0,
+                            PixelFormat.Bgra, PixelType.UnsignedByte, locked.Address);
+                    }
+
+                    Gl.TextureParameterEXT(_textureId, TextureTarget.Texture2d, TextureParameterName.TextureMagFilter,
+                        _ui._filterMode == UI.FilterMode.Linear ? Gl.LINEAR : Gl.NEAREST);
+
+                    Gl.Begin(PrimitiveType.Quads);
+                    Gl.TexCoord2(0, 1);
+                    Gl.Vertex2(0, 0);
+                    Gl.TexCoord2(0, 0);
+                    Gl.Vertex2(0, 1);
+                    Gl.TexCoord2(1, 0);
+                    Gl.Vertex2(1, 1);
+                    Gl.TexCoord2(1, 1);
+                    Gl.Vertex2(1, 0);
+                    Gl.End();
                 }
-
-
-                Gl.TextureParameterEXT(_textureId, TextureTarget.Texture2d, TextureParameterName.TextureMagFilter, _ui._filterMode == UI.FilterMode.Linear ? Gl.LINEAR : Gl.NEAREST);
-
-                Gl.Begin(PrimitiveType.Quads);
-                Gl.TexCoord2(0, 1); Gl.Vertex2(0, 0);
-                Gl.TexCoord2(0, 0); Gl.Vertex2(0, 1);
-                Gl.TexCoord2(1, 0); Gl.Vertex2(1, 1);
-                Gl.TexCoord2(1, 1); Gl.Vertex2(1, 0);
-                Gl.End();
             }
         }
 
@@ -77,20 +82,11 @@ namespace dotNES.Renderers
 
         public void EndRendering()
         {
-            DisposeOpenGL();
-        }
 
-        private void DisposeOpenGL()
-        {
-            if (_ui != null && _ui.ready)
-            {
-                _ui.ready = false;
-            }
         }
-
+        
         protected override void OnResize(EventArgs e)
         {
-            DisposeOpenGL();
             InitRendering(_ui);
             base.OnResize(e);
         }
