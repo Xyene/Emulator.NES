30477a47307d66741fac8c69d4de9b898ad077a3
Tudor Brindus <xyene@dmoj.ca>
2017-09-14 17:27:59 +0200

    Add GxROM support

---
+++ dotNES/Mappers/GxROM.cs
@@ -0,0 +1,29 @@
+﻿namespace dotNES.Mappers
+{
+    [MapperDef(66)]
+    class GxROM : BaseMapper
+    {
+        protected int _prgBankOffset;
+        protected int _chrBankOffset;
+
+        public GxROM(Emulator emulator) : base(emulator)
+        {
+        }
+
+        public override void InitializeMemoryMap(PPU ppu)
+        {
+            ppu.MapReadHandler(0x0000, 0x1FFF, addr => _chrROM[_chrBankOffset + addr]);
+        }
+
+        public override void InitializeMemoryMap(CPU cpu)
+        {
+            cpu.MapReadHandler(0x8000, 0xFFFF, addr => _prgROM[_prgBankOffset + (addr - 0x8000)]);
+
+            cpu.MapWriteHandler(0x8000, 0xFFFF, (addr, val) =>
+            {
+                _prgBankOffset = ((val >> 4) & 0x3) * 0x8000;
+                _chrBankOffset = (val & 0x3) * 0x2000;
+            });
+        }
+    }
+}
